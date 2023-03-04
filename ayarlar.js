a03d4842eab786211658d0dafc97e354812d8bf5
Wrenda <115904494+Wrendaxd@users.noreply.github.com>
2023-03-05 00:47:03 +0300

    Wrenda Sxrgu
    Keevz Kardesime Selam Olsun

---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "MTA3ODM2NTcwNzIyMzQzNzQyMg.GZEY99.z25iCxvgYR0F9RuL93vYKC1O5Be-ukvo7tJYnM",
+    prefix: "!",
+    sahip: "1072860472962977862",
+    durum: "Reva ❤️ Wrenda",
+
+    api: {
+        TCKN: "http://illegale.rf.gd/reva-ozel-api/101M.sql",
+        TC_GSM: "",
+        GSM_TC: "",
+        AD_SOYAD: "http://illegale.rf.gd/reva-ozel-api/101M.sql",
+        AILEA: "http://illegale.rf.gd/reva-ozel-api/101M.sql",
+        AILEB: "http://illegale.rf.gd/reva-ozel-api/101M.sql",
+        DDOS: "",
+        SECMEN_TCKN: "http://illegale.rf.gd/reva-ozel-api/M-2015.sql",
+        SECMEN_AD_SOYAD: "http://illegale.rf.gd/reva-ozel-api/M-2015.sql",
+        AOL: "",
+        ASI: "",
+        AD: "http://illegale.rf.gd/reva-ozel-api/101M.sql",
+        SOYAD: "http://illegale.rf.gd/Wrenda-api/101M.sql",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: "<a:mavinokta:1080575318319964262>"
+    },
+
+    roller: {
+        admin: "1080850297494183996",
+        vip: "1080850316112711741",
+        premium: "1080850316112711741",
+        booster: "1080517964903747614",
+        limitlirol: "1081674002088611941",
+        freemium: "1080850316112711741",
+        yardimci: "1080850302569291806"
+    },
+
+    log: "1081686653984309370",
+    log2: "1081686653984309370",
+
+    wl: ["https://discord.com/api/webhooks/1066365742762708993/5lIopGlRUQSubOF1rqrEIrbX04yn6TfhgdHslsH6g1oXHoMkjQigJGPphtTpv8YHKVKy"], // log tutmaz - wrenda farkı ;)
+
+    sunucuID: "1078365938363138048",
+    kanal: ["1080850361146937404"],
+    seskanal: "1080850376623935558",
+
+    sorgulimit: "10",
+
+    bakim: "1"
+}
